9342c16f229f68271857bce38803dd5e312de408
Rob <120913030+RoT1337@users.noreply.github.com>
2024-09-04 11:14:39 +0800

    Rule Based Expert Systems Activity
    V1

--- src/main/java/expertsystemsactivity/mainbanana.java
+++ src/main/java/expertsystemsactivity/mainbanana.java
@@ -1,5 +1,69 @@
 package expertsystemsactivity;
 
 public class mainbanana {
+    private int str = 0;
+    private int intl = 0;
+    private int dex = 0;
+    private int end = 0;
+    private int cha = 0;
+    private int agi = 0;
+    private int fai = 0;
 
+    public mainbanana(int str, int intl, int dex, int end, int cha, int agi, int fai) {
+        this.str = str;
+        this.intl = intl;
+        this.dex = dex;
+        this.end = end;
+        this.cha = cha;
+        this.agi = agi;
+        this.fai = fai;
+    }
+
+    public mainbanana() {}
+
+    public void setStr(int str) {
+        this.str = str;
+    }
+    public void setIntl(int intl) {
+        this.intl = intl;
+    }
+    public void setDex(int dex) {
+        this.dex = dex;
+    }
+    public void setEnd(int end) {
+        this.end = end;
+    }
+    public void setCha(int cha) {
+        this.cha = cha;
+    }
+    public void setAgi(int agi) {
+        this.agi = agi;
+    }
+    public void setFai(int fai) {
+        this.fai = fai;
+    }
+
+    public int getStr() {
+        return str;
+    }
+    public int getIntl() {
+        return intl;
+    }
+    public int getDex() {
+        return dex;
+    }
+    public int getEnd() {
+        return end;
+    }
+    public int getCha() {
+        return cha;
+    }
+    public int getAgi() {
+        return agi;
+    }
+    public int getFai() {
+        return fai;
+    }
+
+    
 }
--- src/main/java/expertsystemsactivity/rulesandstats.java
+++ src/main/java/expertsystemsactivity/rulesandstats.java
@@ -1,7 +1,53 @@
 package expertsystemsactivity;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class rulesandstats {
+    static ArrayList<String> answers = new ArrayList<>();
     public static void main(String[] args) {
-        System.out.println("Hello Balls!");
+        Scanner input = new Scanner(System.in);
+
+        String answer;
+
+        System.out.println("Welcome to the RPG Class designator.");
+        System.out.println("You will be asked a few quetions and based on your answer, "
+        + "your probable RPG class will be given.");
+        System.out.println("-------------------------------------------------------------------------");
+        System.out.println("Testingsz");
+        System.out.println("-------------------------------------------------------------------------");
+        System.out.print("Do you lift? (Y/N): ");
+        answer = input.nextLine();
+        answers.add(answer);
+        answer = "";
+        System.out.print("Do you read? (Y/N): ");
+        answer = input.nextLine();
+        answers.add(answer);
+        answer = "";
+        System.out.print("Do you run? (Y/N): ");
+        answer = input.nextLine();
+        answers.add(answer);
+        answer = "";
+        System.out.print("When pushed, do you feel like you give up easily? (Y/N): ");
+        answer = input.nextLine();
+        answers.add(answer);
+        answer = "";
+        System.out.print("Do people laugh at your jokes? (Y/N): ");
+        answer = input.nextLine();
+        answers.add(answer);
+        answer = "";
+        System.out.print("Do you climb? (Y/N): ");
+        answer = input.nextLine();
+        answers.add(answer);
+        answer = "";
+        System.out.print("Do you pray? (Y/N): ");
+        answer = input.nextLine();
+        answers.add(answer);
+        answer = "";
+
+        System.out.println("-------------------------------------------------------------------------");
+        for (String ans: answers) {
+            System.out.print(ans + ", ");
+        }
     }
 }
