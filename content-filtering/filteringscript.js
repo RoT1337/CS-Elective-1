1571c1b985333f20c4b5ca2579821f22a44c6083
Rob <120913030+RoT1337@users.noreply.github.com>
2024-10-15 17:43:54 +0800

    Recommender Systems - Collaborative Filtering
    Made the U.I. and added a bit of CSS using SCSS. Will make the code for the filtering using Javascript

---
+++ content-filtering/filteringscript.js
@@ -0,0 +1,14 @@
+const userArr =[[]];
+
+    let name = document.getElementById('userName');
+    let form = document.getElementById('gameForm');
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        check();
+    });
+
+    function check() {
+
+    }
