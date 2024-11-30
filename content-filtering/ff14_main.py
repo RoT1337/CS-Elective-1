e90aed354c8caf83cad826eb5a10325e79a9e5ae
Rob <120913030+RoT1337@users.noreply.github.com>
2024-11-30 16:34:03 +0800

    Update ff14_main.py

--- content-filtering/ff14_main.py
+++ content-filtering/ff14_main.py
@@ -4,6 +4,7 @@ from tkinter import messagebox
 from PIL import Image
 import numpy as np
 from numpy.linalg import norm
+import os
 
 ck.set_appearance_mode("dark")
 ck.set_default_color_theme("dark-blue")
@@ -14,16 +15,16 @@ root.geometry("400x300")
 # count = 0
 
 user_inputs = [
-    ["User1", 50, 0, 75, 20, 0, 60, 0, 80, 0, 90, 0, 70, 0, 85, 0, 95, 0, 50, 0, 60],
-    ["User2", 0, 40, 0, 60, 0, 80, 0, 100, 0, 20, 0, 40, 0, 60, 0, 80, 0, 70, 0, 50],
-    ["User3", 30, 0, 50, 0, 70, 0, 90, 0, 10, 0, 30, 0, 50, 0, 70, 0, 60, 0, 40],
-    ["User4", 0, 20, 0, 40, 0, 60, 0, 80, 0, 100, 0, 20, 0, 40, 0, 60, 0, 50, 0, 30],
-    ["User5", 10, 0, 30, 0, 50, 0, 70, 0, 90, 0, 10, 0, 30, 0, 50, 0, 40, 0, 20],
-    ["User6", 0, 60, 0, 80, 0, 100, 0, 20, 0, 40, 0, 60, 0, 80, 0, 100, 0, 90, 0, 70],
-    ["User7", 70, 0, 90, 0, 10, 0, 30, 0, 50, 0, 70, 0, 90, 0, 10, 0, 80, 0, 60],
-    ["User8", 0, 50, 0, 70, 0, 90, 0, 10, 0, 30, 0, 50, 0, 70, 0, 90, 0, 100, 0, 80],
-    ["User9", 20, 0, 40, 0, 60, 0, 80, 0, 100, 0, 20, 0, 40, 0, 60, 0, 70, 0, 50],
-    ["User10", 0, 30, 0, 50, 0, 70, 0, 90, 0, 10, 0, 30, 0, 50, 0, 60, 0, 40, 0, 20]
+    ["User1", 45, 0, 78, 22, 0, 63, 0, 81, 0, 92, 0, 71, 0, 87, 0, 96, 0, 53, 0, 61],
+    ["User2", 0, 42, 0, 65, 0, 83, 0, 99, 0, 25, 0, 43, 0, 67, 0, 82, 0, 73, 0, 55],
+    ["User3", 33, 0, 52, 0, 74, 0, 91, 0, 15, 0, 35, 0, 54, 0, 72, 0, 64, 0, 45],
+    ["User4", 0, 24, 0, 47, 0, 68, 0, 85, 0, 100, 0, 23, 0, 41, 0, 66, 0, 57, 0, 38],
+    ["User5", 12, 0, 34, 0, 56, 0, 77, 0, 93, 0, 14, 0, 32, 0, 51, 0, 48, 0, 29],
+    ["User6", 0, 61, 0, 79, 0, 98, 0, 21, 0, 44, 0, 62, 0, 84, 0, 97, 0, 91, 0, 72],
+    ["User7", 73, 0, 89, 0, 13, 0, 31, 0, 53, 0, 75, 0, 88, 0, 19, 0, 81, 0, 63],
+    ["User8", 0, 55, 0, 71, 0, 94, 0, 17, 0, 36, 0, 58, 0, 76, 0, 95, 0, 99, 0, 83],
+    ["User9", 27, 0, 49, 0, 67, 0, 86, 0, 100, 0, 22, 0, 46, 0, 65, 0, 74, 0, 52],
+    ["User10", 0, 37, 0, 59, 0, 80, 0, 92, 0, 11, 0, 33, 0, 50, 0, 69, 0, 47, 0, 28]
 ]
 
 # 20 Jobs
@@ -47,6 +48,43 @@ white_mage_level_var = tk.StringVar()
 scholar_level_var = tk.StringVar()
 astrologian_level_var = tk.StringVar()
 sage_level_var = tk.StringVar()
+
+    # List of job names
+job_names = [
+        "Paladin", "Warrior", "Dark Knight", "Gunbreaker",
+        "Dragoon", "Reaper", "Monk", "Samurai", "Ninja", "Viper",
+        "Bard", "Machinist", "Dancer", "Black Mage", "Summoner",
+        "Red Mage", "White Mage", "Scholar", "Astrologian", "Sage"
+    ]
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+tank_base_dir = os.path.join(script_dir, "JobIcons/01_TANK/Job")
+dps_base_dir = os.path.join(script_dir, "JobIcons/03_DPS/Job")
+healer_base_dir = os.path.join(script_dir, "JobIcons/02_Healer/Job")
+
+paladin_path = os.path.join(tank_base_dir, "Paladin.png")
+warrior_path = os.path.join(tank_base_dir, "Warrior.png")
+dark_knight_path = os.path.join(tank_base_dir, "DarkKnight.png")
+gunbreaker_path = os.path.join(tank_base_dir, "Gunbreaker.png")
+
+dragoon_path = os.path.join(dps_base_dir, "Dragoon.png")
+reaper_path = os.path.join(dps_base_dir, "Reaper.png")
+monk_path = os.path.join(dps_base_dir, "Monk.png")
+samurai_path = os.path.join(dps_base_dir, "Samurai.png")
+ninja_path = os.path.join(dps_base_dir, "Ninja.png")
+viper_path = os.path.join(dps_base_dir, "Viper.png")
+bard_path = os.path.join(dps_base_dir, "Bard.png")
+machinist_path = os.path.join(dps_base_dir, "Machinist.png")
+dancer_path = os.path.join(dps_base_dir, "Dancer.png")
+black_mage_path = os.path.join(dps_base_dir, "BlackMage.png")
+summoner_path = os.path.join(dps_base_dir, "Summoner.png")
+red_mage_path = os.path.join(dps_base_dir, "RedMage.png")
+white_mage_path = os.path.join(healer_base_dir, "WhiteMage.png")
+
+scholar_path = os.path.join(healer_base_dir, "Scholar.png")
+astrologian_path = os.path.join(healer_base_dir, "Astrologian.png")
+sage_path = os.path.join(healer_base_dir, "Sage.png")
 
 def validate_number_input(event):
     entry = event.widget
@@ -90,10 +128,10 @@ def open_tank_window():
     label = ck.CTkLabel(master=tank_window, text="Input your Warrior of Light's Tank Levels")
     label.pack(pady=12, padx=10)
 
-    create_input_frame(tank_window, "C:/Computer Science/3rdYear/CS-Elective-1/content-filtering/JobIcons/01_TANK/Job/Paladin.png", paladin_level_var)
-    create_input_frame(tank_window, "C:/Computer Science/3rdYear/CS-Elective-1/content-filtering/JobIcons/01_TANK/Job/Warrior.png", warrior_level_var)
-    create_input_frame(tank_window, "C:/Computer Science/3rdYear/CS-Elective-1/content-filtering/JobIcons/01_TANK/Job/DarkKnight.png", dark_knight_level_var)
-    create_input_frame(tank_window, "C:/Computer Science/3rdYear/CS-Elective-1/content-filtering/JobIcons/01_TANK/Job/Gunbreaker.png", gunbreaker_level_var)
+    create_input_frame(tank_window, paladin_path, paladin_level_var)
+    create_input_frame(tank_window, warrior_path, warrior_level_var)
+    create_input_frame(tank_window, dark_knight_path, dark_knight_level_var)
+    create_input_frame(tank_window, gunbreaker_path, gunbreaker_level_var)
 
     tank_button = ck.CTkButton(master=tank_window, text="Continue", command=open_meleedps_window)
     tank_button.pack(pady=12, padx=10)
@@ -103,7 +141,7 @@ def open_meleedps_window():
     tank_window.destroy()
     meleedps_window = ck.CTkToplevel(root)
     window_width = 400
-    window_height = 400
+    window_height = 500
     screen_width = meleedps_window.winfo_screenwidth()
     screen_height = meleedps_window.winfo_screenheight()
     position_top = int(screen_height / 2 - window_height / 2)
@@ -114,12 +152,12 @@ def open_meleedps_window():
     label = ck.CTkLabel(master=meleedps_window, text="Input your Warrior of Light's Melee DPS Levels")
     label.pack(pady=12, padx=10)
 
-    create_input_frame(meleedps_window, "C:/Computer Science/3rdYear/CS-Elective-1/content-filtering/JobIcons/03_DPS/Job/Dragoon.png", dragoon_level_var)
-    create_input_frame(meleedps_window, "C:/Computer Science/3rdYear/CS-Elective-1/content-filtering/JobIcons/03_DPS/Job/Reaper.png", reaper_level_var)
-    create_input_frame(meleedps_window, "C:/Computer Science/3rdYear/CS-Elective-1/content-filtering/JobIcons/03_DPS/Job/Monk.png", monk_level_var)
-    create_input_frame(meleedps_window, "C:/Computer Science/3rdYear/CS-Elective-1/content-filtering/JobIcons/03_DPS/Job/Samurai.png", samurai_level_var)
-    create_input_frame(meleedps_window, "C:/Computer Science/3rdYear/CS-Elective-1/content-filtering/JobIcons/03_DPS/Job/Ninja.png", ninja_level_var)
-    create_input_frame(meleedps_window, "C:/Computer Science/3rdYear/CS-Elective-1/content-filtering/JobIcons/03_DPS/Job/Viper.png", viper_level_var)
+    create_input_frame(meleedps_window, dragoon_path, dragoon_level_var)
+    create_input_frame(meleedps_window, reaper_path, reaper_level_var)
+    create_input_frame(meleedps_window, monk_path, monk_level_var)
+    create_input_frame(meleedps_window, samurai_path, samurai_level_var)
+    create_input_frame(meleedps_window, ninja_path, ninja_level_var)
+    create_input_frame(meleedps_window, viper_path, viper_level_var)
 
     meleedps_button = ck.CTkButton(master=meleedps_window, text="Submit", command=open_physrange_dps)
     meleedps_button.pack(pady=12, padx=10)
@@ -140,9 +178,9 @@ def open_physrange_dps():
     label = ck.CTkLabel(master=physrange_window, text="Input your Warrior of Light's Physical Ranged DPS Levels")
     label.pack(pady=12, padx=10)
 
-    create_input_frame(physrange_window, "C:/Computer Science/3rdYear/CS-Elective-1/content-filtering/JobIcons/03_DPS/Job/Bard.png", bard_level_var)
-    create_input_frame(physrange_window, "C:/Computer Science/3rdYear/CS-Elective-1/content-filtering/JobIcons/03_DPS/Job/Machinist.png", machinist_level_var)
-    create_input_frame(physrange_window, "C:/Computer Science/3rdYear/CS-Elective-1/content-filtering/JobIcons/03_DPS/Job/Dancer.png", dancer_level_var)
+    create_input_frame(physrange_window, bard_path, bard_level_var)
+    create_input_frame(physrange_window, machinist_path, machinist_level_var)
+    create_input_frame(physrange_window, dancer_path, dancer_level_var)
 
     physrange_button = ck.CTkButton(master=physrange_window, text="Submit", command=open_magirange_dps)
     physrange_button.pack(pady=12, padx=10)
@@ -163,9 +201,9 @@ def open_magirange_dps():
     label = ck.CTkLabel(master=magirange_window, text="Input your Warrior of Light's Magical Ranged DPS Levels")
     label.pack(pady=12, padx=10)
 
-    create_input_frame(magirange_window, "C:/Computer Science/3rdYear/CS-Elective-1/content-filtering/JobIcons/03_DPS/Job/BlackMage.png", black_mage_level_var)
-    create_input_frame(magirange_window, "C:/Computer Science/3rdYear/CS-Elective-1/content-filtering/JobIcons/03_DPS/Job/Summoner.png", summoner_level_var)
-    create_input_frame(magirange_window, "C:/Computer Science/3rdYear/CS-Elective-1/content-filtering/JobIcons/03_DPS/Job/RedMage.png", red_mage_level_var)
+    create_input_frame(magirange_window, black_mage_path, black_mage_level_var)
+    create_input_frame(magirange_window, summoner_path, summoner_level_var)
+    create_input_frame(magirange_window, red_mage_path, red_mage_level_var)
 
     magirange_button = ck.CTkButton(master=magirange_window, text="Submit", command=open_healer_window)
     magirange_button.pack(pady=12, padx=10)
@@ -186,10 +224,10 @@ def open_healer_window():
     label = ck.CTkLabel(master=healer_window, text="Input your Warrior of Light's Healer Levels")
     label.pack(pady=12, padx=10)
 
-    create_input_frame(healer_window, "C:/Computer Science/3rdYear/CS-Elective-1/content-filtering/JobIcons/02_HEALER/Job/WhiteMage.png", white_mage_level_var)
-    create_input_frame(healer_window, "C:/Computer Science/3rdYear/CS-Elective-1/content-filtering/JobIcons/02_HEALER/Job/Scholar.png", scholar_level_var)
-    create_input_frame(healer_window, "C:/Computer Science/3rdYear/CS-Elective-1/content-filtering/JobIcons/02_HEALER/Job/Astrologian.png", astrologian_level_var)
-    create_input_frame(healer_window, "C:/Computer Science/3rdYear/CS-Elective-1/content-filtering/JobIcons/02_HEALER/Job/Sage.png", sage_level_var)
+    create_input_frame(healer_window, white_mage_path, white_mage_level_var)
+    create_input_frame(healer_window, scholar_path, scholar_level_var)
+    create_input_frame(healer_window, astrologian_path, astrologian_level_var)
+    create_input_frame(healer_window, sage_path, sage_level_var)
 
     healer_button = ck.CTkButton(master=healer_window, text="Submit", command=finish)
     healer_button.pack(pady=12, padx=10)
@@ -213,7 +251,7 @@ def finish():
     global entry2
     entry2 = ck.CTkEntry(master=new_frame, placeholder_text="Input Name")
     entry2.pack(pady=12, padx=10)
-    button1 = ck.CTkButton(master=new_frame, text="Recommend me a Class", hover_color="green", command=lambda: recommend_next_job(entry2.get()))
+    button1 = ck.CTkButton(master=new_frame, text="Recommend me a Job", hover_color="green", command=lambda: recommend_next_job(entry2.get()))
     button1.pack(pady=12, padx=10)
     root.geometry("400x450")
 
@@ -301,14 +339,6 @@ def calculate_cosine_similarity(user_name):
     return similarity_scores
 
 def recommend_next_job(user_name):
-    # List of job names corresponding to the indices
-    job_names = [
-        "Paladin", "Warrior", "Dark Knight", "Gunbreaker",
-        "Dragoon", "Reaper", "Monk", "Samurai", "Ninja", "Viper",
-        "Bard", "Machinist", "Dancer", "Black Mage", "Summoner",
-        "Red Mage", "White Mage", "Scholar", "Astrologian", "Sage"
-    ]
-
     # Find the target user data
     target_user_data = None
     for user_data in user_inputs:
@@ -335,7 +365,7 @@ def recommend_next_job(user_name):
             for user_data in user_inputs:
                 if user_data[0] == name:
                     if len(user_data) > job_index + 1 and user_data[job_index + 1] > 0:  # Adjust index to account for the user name
-                        numerator += similarity * user_data[job_index + 1]
+                        numerator += similarity * user_data[job_index + 1] 
                         denominator += similarity
         if denominator > 0:
             recommended_rating = numerator / denominator
@@ -349,7 +379,31 @@ def recommend_next_job(user_name):
         job_name = job_names[job_index - 1]  # Adjust index if needed
         print(f"{job_name}: Recommended rating {rating}")
     
-    return recommended_jobs
+    # Create a frame to the right of the root window
+    right_frame = ck.CTkFrame(master=root)
+    right_frame.pack(side="right", pady=10, padx=10, fill="both", expand=True)
+
+    # Display top 3 closest similarity users
+    top_users_label = ck.CTkLabel(master=right_frame, text="Top 3 Closest Similarity Users")
+    top_users_label.pack(pady=5, padx=5)
+
+    # Get the top 3 users
+    top_users = [(name, round(score, 2)) for name, score in similarity_scores[:3]]
+    for user, score in top_users:
+        user_label = ck.CTkLabel(master=right_frame, text=f"{user}: Similarity score {score}")
+        user_label.pack(pady=2, padx=5)
+
+    # Display top 3 suggested jobs
+    top_jobs_label = ck.CTkLabel(master=right_frame, text="Top 3 Suggested Jobs")
+    top_jobs_label.pack(pady=5, padx=5)
+
+    # Get the top 3 recommended jobs
+    top_jobs = [(job_names[job_index - 1], round(rating, 2)) for job_index, rating in recommended_jobs[:3]]
+    for job_name, rating in top_jobs:
+        job_label = ck.CTkLabel(master=right_frame, text=f"{job_name}: Recommended rating {rating}")
+        job_label.pack(pady=2, padx=5)
+
+    root.geometry("400x650")
 
 window_width = 400
 window_height = 300
